790ef95247c8a0a182bdb1a507ebb9701be76695
stevennight <stevennight@qq.com>
2020-04-26 12:17:02 +0800

    404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,12 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: '404.html',
+    mounted() {
+        location.href = '/custom/404.html';
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import VueRouter from 'vue-router';
 //pages
 import IndexComponent from '@/pages/index/Index'
 import ReaderComponent from '@/pages/reader/Reader'
+import NotFoundComponent from '@/pages/404'
 
 Vue.use(VueRouter);
 
 let routes = [
     {path: '/', component: IndexComponent},
-    {path: '/reader', component: ReaderComponent}
+    {path: '/reader', component: ReaderComponent},
+    {path: '*', component: NotFoundComponent}
 ];
 let router = new VueRouter({
     routes
